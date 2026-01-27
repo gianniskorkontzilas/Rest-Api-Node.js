120b5a8b4149d9266d59cc3483fc181d2deec97c
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-27 19:43:15 +0200

    Create Controlles, simplify routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,39 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const {getContacts, createContact, getContact, updateContact, deleteContact} = require("../controllers/contactController")
 
-router.route("/").get ((req, res) => {
-    // res.send("Get all contacts");    
-    // res.json({message:"Get all contacts"});
-    res.status(200).json({message:"Get all contacts"});
+router.route("/").get (getContacts).post(createContact);
 
-});
+// router.route("/").post (createContact);
 
-router.route("/").post ((req, res) => {
-    // res.send("Get all contacts");    
-    // res.json({message:"Get all contacts"});
-    res.status(200).json({message:"Create contact"});
+router.route("/:id").get (getContact).put (updateContact).delete(deleteContact);
 
-});
-router.route("/:id").get ((req, res) => {
-    // res.send("Get all contacts");    
-    // res.json({message:"Get all contacts"});
-    res.status(200).json({message:`Get contact for ${req.params.id}`});
 
-});
+// router.route("/:id").put (updateContact);
 
-router.route("/:id").put ((req, res) => {
-    // res.send("Get all contacts");    
-    // res.json({message:"Get all contacts"});
-    res.status(200).json({message:`update contact for ${req.params.id}`});
 
-});
-
-router.route("/:id").delete ((req, res) => {
-    // res.send("Get all contacts");    
-    // res.json({message:"Get all contacts"});
-    res.status(200).json({message:`Delete contact for ${req.params.id}`});
-
-});
+// router.route("/:id").delete (deleteContact);
+   
 
 
 module.exports = router;
