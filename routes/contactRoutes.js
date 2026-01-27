85bbdabb88020a454dc94b099646358baab0f644
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-27 18:48:49 +0200

    Create routes

---
+++ routes/contactRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get ((req, res) => {
+    // res.send("Get all contacts");    
+    // res.json({message:"Get all contacts"});
+    res.status(200).json({message:"Get all contacts"});
+
+});
+
+router.route("/").post ((req, res) => {
+    // res.send("Get all contacts");    
+    // res.json({message:"Get all contacts"});
+    res.status(200).json({message:"Create contact"});
+
+});
+router.route("/:id").get ((req, res) => {
+    // res.send("Get all contacts");    
+    // res.json({message:"Get all contacts"});
+    res.status(200).json({message:`Get contact for ${req.params.id}`});
+
+});
+
+router.route("/:id").put ((req, res) => {
+    // res.send("Get all contacts");    
+    // res.json({message:"Get all contacts"});
+    res.status(200).json({message:`update contact for ${req.params.id}`});
+
+});
+
+router.route("/:id").delete ((req, res) => {
+    // res.send("Get all contacts");    
+    // res.json({message:"Get all contacts"});
+    res.status(200).json({message:`Delete contact for ${req.params.id}`});
+
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenev = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+})
