88d2381465dd2a0646baab3d978497ebe75d2877
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-28 20:14:04 +0200

    Built-in Midlleware for POST

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const dotenev = require("dotenv").config();
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"))
 
 app.listen(port, () => {
